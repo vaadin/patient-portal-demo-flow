d342cfb5164fe50a570d8a62a0c1304a6e1d583d
Pekka Maanpaa <pekkamaa@vaadin.com>
2017-06-14 13:57:22 +0300

    Add a starting point for the Flow-based Patient Portal application.
    The project is based on the flow-hello-world project. Templates are added from the patient-portal-demo-polymer project and PatientPortal.java provides an entry-point into those tempates. So currently it just runs the Polymer-project without really utilizing Java and Vaadin Flow.

---
+++ .travis.script.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# TRAVIS_PULL_REQUEST == "false" for a normal branch commit, the PR number for a PR
+# TRAVIS_BRANCH == target of normal commit or target of PR
+# TRAVIS_SECURE_ENV_VARS == true if encrypted variables, e.g. SONAR_HOST is available
+# TRAVIS_REPO_SLUG == the repository, e.g. vaadin/vaadin
+
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+mvn -B -e -V -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE clean verify
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# patient-portal-demo-flow
+# Patient Portal UI
+
+This is the UI for the Patient Portal demo app made with Vaadin Flow using Polymer templates.
+The flow-hello-world project has been used as a starting point for this project.
+
+Run using
+```mvn clean jetty:run```
