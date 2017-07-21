2c7b2b23df7326013ee300e08c035390681bfc5e
Pekka Maanpää <pekkamaa@vaadin.com>
2017-07-21 09:20:30 +0300

    Populate patients-grid with all necessary data from server-side. (#36)
    Before this only the names of patients were sent to the client. Now every column of the grid is populated with data that is converted into supported types.
    
    Couldn't access backend from the test-case so patient's name and id are hard-coded and for other values only the correct format is tested because the values change between server-sessions.
    This commit also creates separate classes for test-cases and introduces a common superclass for them.

---
+++ src/main/java/com/vaadin/flow/demo/patientportal/converters/DateToStringConverter.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.demo.patientportal.converters;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.vaadin.flow.template.model.ModelConverter;
+
+/**
+ * Converts between Date-objects and their String-representations in format
+ * 'yyyy/MM/dd'.
+ * 
+ * @author Vaadin Ltd
+ *
+ */
+
+public class DateToStringConverter implements ModelConverter<Date, String> {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat(
+            "yyyy/MM/dd");
+
+    @Override
+    public String toModel(Date applicationValue) {
+        return applicationValue == null ? null
+                : dateFormat.format(applicationValue);
+    }
+
+    @Override
+    public Date toApplication(String modelValue) {
+        try {
+            return dateFormat.parse(modelValue);
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+
+}
---
+++ src/test/java/com/vaadin/flow/demo/patientportal/PatientsViewIT.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.demo.patientportal;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+/**
+ * @author Vaadin Ltd
+ *
+ */
+public class PatientsViewIT extends AbstractChromeTest {
+
+    @Override
+    protected String getTestPath() {
+        return "/patients";
+    }
+
+    @Test
+    public void testPatientData() {
+        open();
+
+        Assert.assertThat(
+                "Grid should contain the first name of the first patient.",
+                getCellText(13), containsString("Frederick"));
+        Assert.assertThat(
+                "Grid should contain the last name of the first patient.",
+                getCellText(13), containsString("Wilson"));
+
+        Assert.assertThat("Grid should contain the id of the first patient.",
+                getCellText(14), is("1"));
+
+        String medicalRecord = getCellText(15);
+        Assert.assertTrue(
+                "Grid should contain the medical record of the first patient.\n"
+                        + "Expected: an integer value\n" + "Actual value: '"
+                        + medicalRecord + "'",
+                medicalRecord.matches("\\d+"));
+
+        String doctorName = getCellText(16);
+        Assert.assertTrue(
+                "Grid should contain the name of the first patient's doctor.\n"
+                        + "Expected format: Lastname, Firstname\n"
+                        + "Actual value: '" + doctorName + "'",
+                doctorName.matches("\\w+, \\w+"));
+
+        String lastVisit = getCellText(17);
+        Assert.assertTrue(
+                "Grid should contain the last visit date of the first patient.\n"
+                        + "Expected format: yyyy/MM/dd or an empty string\n"
+                        + "Actual value: '" + lastVisit + "'",
+                lastVisit.matches("(\\d{4}/\\d{2}/\\d{2})|^$"));
+
+    }
+
+    private WebElement getGridCell(int id) {
+        return getInShadowRoot(By.tagName("patients-view"),
+                By.id("vaadin-grid-cell-content-" + id));
+    }
+
+    private String getCellText(int id) {
+        return getGridCell(id).getText();
+    }
+}
