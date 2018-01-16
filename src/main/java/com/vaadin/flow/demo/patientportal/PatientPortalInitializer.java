3db171f794176b29424643c93bdf6016f8e50d2f
elmot <elmot@vaadin.com>
2018-01-16 11:36:34 +0200

    Spring settings optimized

--- src/main/java/com/vaadin/flow/demo/patientportal/PatientPortalInitializer.java
+++ src/main/java/com/vaadin/flow/demo/patientportal/PatientPortalInitializer.java
@@ -15,13 +15,21 @@
  */
 package com.vaadin.flow.demo.patientportal;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.WebApplicationInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.vaadin.demo.service.DBInitService;
 
 /**
  * Spring boot web appplication initializer.
@@ -30,20 +38,29 @@ import org.springframework.web.WebApplicationInitializer;
  * @author Vaadin Ltd
  */
 @SpringBootApplication
-/*
- * To disable we security:
- *
- * @SpringBootApplication(exclude = {
- * org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.
- * class })
- */
-@EnableAutoConfiguration(exclude = {WebMvcAutoConfiguration.class,
-        SpringDataWebAutoConfiguration.class})
-public class PatientPortalInitializer extends SpringBootServletInitializer
-        implements WebApplicationInitializer {
+@EnableAutoConfiguration(exclude = { WebMvcAutoConfiguration.class,
+        SpringDataWebAutoConfiguration.class, })
+@ComponentScan({ "com.vaadin.flow.demo.patientportal", "com.vaadin.demo" })
+@EnableJpaRepositories("com.vaadin.demo.repositories")
+@EntityScan("com.vaadin.demo.entities")
+public class PatientPortalInitializer extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(PatientPortalInitializer.class, args);
+    }
+
+    @Autowired
+    private DBInitService initService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/*").permitAll();
+    }
+
+    @PostConstruct
+    private void init() {
+        initService.initDatabase();
     }
 
 }
