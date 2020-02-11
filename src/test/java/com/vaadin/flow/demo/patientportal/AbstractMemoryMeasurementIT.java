d2c1e45d3304431c68037614b740839ceaf2693a
Mikael Grankvist <mikael.grankvist@vaadin.com>
2020-02-11 09:11:10 +0200

    Add logging
    for better knowledge that the test
    is actually still running.
    
    Small change to stability check.

--- src/test/java/com/vaadin/flow/demo/patientportal/AbstractMemoryMeasurementIT.java
+++ src/test/java/com/vaadin/flow/demo/patientportal/AbstractMemoryMeasurementIT.java
@@ -23,6 +23,8 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.vaadin.flow.demo.testcategory.Measurement;
 
@@ -33,12 +35,15 @@ public abstract class AbstractMemoryMeasurementIT extends AbstractChromeTest {
 
     private static final double FAILURE_THRESHOLD = 5 * 0.01; // 5%
 
+    private static final Logger logger = LoggerFactory.getLogger("Measurement");
+    
     @Test
     public void memoryConsumption() {
         long previous = 0;
         long uiSize = 0;
         List<Long> sizes = new ArrayList<>();
         int i = 1;
+        logger.info("Starting generation of ui's to get size");
         while (!isStable(uiSize, sizes)) {
             sizes.add(uiSize);
 
@@ -48,6 +53,7 @@ public abstract class AbstractMemoryMeasurementIT extends AbstractChromeTest {
             previous = current;
             i++;
         }
+        logger.info("Generated {} uis for size comparison.", i);
 
         printTeamcityStats(uiSize);
 
@@ -69,20 +75,29 @@ public abstract class AbstractMemoryMeasurementIT extends AbstractChromeTest {
         for (int i = sizes.size() - 1; i >= sizes.size() - UIS_NUMBER; i--) {
             long previous = sizes.get(i);
             if (Math.abs(size - previous) >= previous * FAILURE_THRESHOLD) {
+                logger.info("UI size still unstable: {} >= {} against ui {}",
+                        Math.abs(size - previous), previous * FAILURE_THRESHOLD,
+                        i);
+                // Clear sizes and only add last so we run up to 5 uis
+                // before retesting as the probablility of hitting Threshold
+                // for all 5 old ones is minimal
+                sizes.clear();
+                sizes.add(size);
                 return false;
             }
         }
+        logger.info("Got stable ui size with {} uis", sizes.size());
         return true;
     }
 
     private long getMemory(int uisAmount) {
         waitForElementPresent(By.id("show-memory"));
         findElement(By.id("show-memory")).click();
-        waitForElementPresent(By.id("memory"));
         WebElement uis = findElement(By.id("uis"));
 
         Assert.assertEquals(String.valueOf(uisAmount), uis.getText());
 
+        waitForElementPresent(By.id("memory"));
         WebElement sessionMemory = findElement(By.id("memory"));
         return Long.parseLong(sessionMemory.getText());
     }
